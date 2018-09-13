ffc6d40af4436cf666d8a2a07abaa0d5e1494bfd
Сидоров Александр Валерьевич <sidorovav@gmail.com>
2018-09-13 09:44:48 +0300

    senk's

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Андрей, спасибо, что помог найти этот сервис!');
 });
 
 app.listen(3000, function () {
